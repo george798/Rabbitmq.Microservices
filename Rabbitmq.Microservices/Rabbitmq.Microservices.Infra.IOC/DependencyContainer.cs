b398a0b2d932d1ed019362e136841780b9c615e1
georgekal <giorgoskal721@gmail.com>
2023-03-08 22:28:21 +0200

    DependencyContainer

---
+++ Rabbitmq.Microservices/Rabbitmq.Microservices.Infra.IOC/DependencyContainer.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Rabbitmq.Microservices.Domain.Core.Bus;
+using Rabbitmq.Microservices.Infra.Bus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rabbitmq.Microservices.Infra.IOC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services) 
+        {
+            //Domain bus
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
